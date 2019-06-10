f84dcbefb071dca84cc26bdf8c0dd1d3c541b2b3
unknown <kpnba90@gmail.com>
2019-06-10 12:50:21 -0400

    finished up to string splitter

---
+++ DSA-recursion.js
@@ -0,0 +1,53 @@
+function countSheep(num) {
+  if (num === 0) {
+    console.log('All sheep jumped over the fence');
+    return '';
+  }
+  console.log(`${num}: Another sheep jumped over the fence`);
+  countSheep(num - 1);
+}
+// countSheep(3);
+
+
+function powerCalculator(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  const exponent = base * powerCalculator(base, exp - 1);
+  return exponent;
+}
+// console.log(powerCalculator(2, 7));
+
+
+function revString(str) {
+  if (str === '') {
+    return '';
+  }
+  const newChar = str[str.length - 1];
+  return newChar + revString(str.slice(0, str.length - 1));
+}
+// console.log(revString('hello'));
+
+
+function triangularNumber(base) {
+  if (base === 1) {
+    return 1;
+  }
+  return base + triangularNumber(base - 1);
+}
+// console.log(triangularNumber(4));
+// console.log(triangularNumber(5));
+
+
+function stringSplitter(str, limit) {
+  if (str === '') {
+    return '';
+  }
+  const newChar = str[0];
+  if (str[0] === limit) {
+    return stringSplitter(str.slice(1), limit);
+  } else {
+    return newChar + stringSplitter(str.slice(1), limit);
+  }
+}
+console.log(stringSplitter('0/2/2/0/20/2/0', '/'));
--- countSheep.js
+++
@@ -1,9 +0,0 @@
-function countSheep(num) {
-  if (num === 0) {
-    console.log('All sheep jumped over the fence');
-    return '';
-  }
-  console.log(`${num}: Another sheep jumped over the fence`);
-  countSheep(num - 1);
-}
-countSheep(3);
